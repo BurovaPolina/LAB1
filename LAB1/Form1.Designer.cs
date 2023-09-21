b9d2431a8184f93b332193671007b656778bc55b
BurovaPolina <Brvbp557122@gmail.com>
2023-09-21 14:26:12 +0300

    Испрвление MaxValue. Стили панели

--- LAB1/Form1.Designer.cs
+++ LAB1/Form1.Designer.cs
@@ -28,77 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.label_float = new System.Windows.Forms.Label();
-			this.label_int = new System.Windows.Forms.Label();
-			this.label_long = new System.Windows.Forms.Label();
-			this.label_ushort = new System.Windows.Forms.Label();
-			this.panel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.label_float);
-			this.panel1.Controls.Add(this.label_int);
-			this.panel1.Controls.Add(this.label_long);
-			this.panel1.Controls.Add(this.label_ushort);
-			this.panel1.Location = new System.Drawing.Point(60, 12);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(200, 100);
-			this.panel1.TabIndex = 0;
-			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-			// 
-			// label_float
-			// 
-			this.label_float.AutoSize = true;
-			this.label_float.Location = new System.Drawing.Point(29, 68);
-			this.label_float.Name = "label_float";
-			this.label_float.Size = new System.Drawing.Size(31, 15);
-			this.label_float.TabIndex = 3;
-			this.label_float.Text = "float";
-			this.label_float.Click += new System.EventHandler(this.label4_Click);
-			// 
-			// label_int
-			// 
-			this.label_int.AutoSize = true;
-			this.label_int.Location = new System.Drawing.Point(29, 53);
-			this.label_int.Name = "label_int";
-			this.label_int.Size = new System.Drawing.Size(21, 15);
-			this.label_int.TabIndex = 2;
-			this.label_int.Text = "int";
-			this.label_int.Click += new System.EventHandler(this.label3_Click);
-			// 
-			// label_long
-			// 
-			this.label_long.AutoSize = true;
-			this.label_long.Location = new System.Drawing.Point(26, 30);
-			this.label_long.Name = "label_long";
-			this.label_long.Size = new System.Drawing.Size(31, 15);
-			this.label_long.TabIndex = 1;
-			this.label_long.Text = "long";
-			this.label_long.Click += new System.EventHandler(this.label2_Click);
-			// 
-			// label_ushort
-			// 
-			this.label_ushort.AutoSize = true;
-			this.label_ushort.Location = new System.Drawing.Point(18, 12);
-			this.label_ushort.Name = "label_ushort";
-			this.label_ushort.Size = new System.Drawing.Size(41, 15);
-			this.label_ushort.TabIndex = 0;
-			this.label_ushort.Text = "ushort";
-			this.label_ushort.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.panel1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
-			this.ResumeLayout(false);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_float = new System.Windows.Forms.Label();
+            this.label_int = new System.Windows.Forms.Label();
+            this.label_long = new System.Windows.Forms.Label();
+            this.label_ushort = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label_float);
+            this.panel1.Controls.Add(this.label_int);
+            this.panel1.Controls.Add(this.label_long);
+            this.panel1.Controls.Add(this.label_ushort);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 220);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label_float
+            // 
+            this.label_float.AutoSize = true;
+            this.label_float.Location = new System.Drawing.Point(3, 72);
+            this.label_float.Name = "label_float";
+            this.label_float.Size = new System.Drawing.Size(31, 15);
+            this.label_float.TabIndex = 3;
+            this.label_float.Text = "float";
+            this.label_float.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label_int
+            // 
+            this.label_int.AutoSize = true;
+            this.label_int.Location = new System.Drawing.Point(3, 48);
+            this.label_int.Name = "label_int";
+            this.label_int.Size = new System.Drawing.Size(21, 15);
+            this.label_int.TabIndex = 2;
+            this.label_int.Text = "int";
+            this.label_int.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label_long
+            // 
+            this.label_long.AutoSize = true;
+            this.label_long.Location = new System.Drawing.Point(3, 24);
+            this.label_long.Name = "label_long";
+            this.label_long.Size = new System.Drawing.Size(31, 15);
+            this.label_long.TabIndex = 1;
+            this.label_long.Text = "long";
+            this.label_long.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label_ushort
+            // 
+            this.label_ushort.AutoSize = true;
+            this.label_ushort.Location = new System.Drawing.Point(3, 0);
+            this.label_ushort.Name = "label_ushort";
+            this.label_ushort.Size = new System.Drawing.Size(41, 15);
+            this.label_ushort.TabIndex = 0;
+            this.label_ushort.Text = "ushort";
+            this.label_ushort.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
 
 		}
 
--- LAB1/Form1.cs
+++ LAB1/Form1.cs
@@ -14,17 +14,17 @@ namespace LAB1
 
 		private void label1_Click(object sender, EventArgs e)
 		{
-			label_ushort.Text = ushort.MinValue + "..." + ushort.MinValue;
+			label_ushort.Text = ushort.MinValue + "..." + ushort.MaxValue;
 		}
 
 		private void label2_Click(object sender, EventArgs e)
 		{
-			label_long.Text = long.MinValue + "..." + long.MinValue;
+			label_long.Text = long.MinValue + "..." + long.MaxValue;
 		}
 
 		private void label3_Click(object sender, EventArgs e)
 		{
-			label_int.Text = int.MinValue + "..." + int.MinValue;
+			label_int.Text = int.MinValue + "..." + int.MaxValue;
 		}
 
 		private void label4_Click(object sender, EventArgs e)
