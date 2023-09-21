c96d21f1694e39f56595bf195be30780d0d85256
BurovaPolina <Brvbp557122@gmail.com>
2023-09-21 14:19:10 +0300

    Исправление, перенос значений в функциях

--- LAB1/Form1.cs
+++ LAB1/Form1.cs
@@ -14,22 +14,22 @@ namespace LAB1
 
 		private void label1_Click(object sender, EventArgs e)
 		{
-			label_ushort.Text = long.MinValue + "..." + long.MinValue;
+			label_ushort.Text = ushort.MinValue + "..." + ushort.MinValue;
 		}
 
 		private void label2_Click(object sender, EventArgs e)
 		{
-			label_long.Text = int.MinValue + "..." + int.MinValue;
+			label_long.Text = long.MinValue + "..." + long.MinValue;
 		}
 
 		private void label3_Click(object sender, EventArgs e)
 		{
-			label_int.Text = float.MinValue + "..." int.MinValue;
+			label_int.Text = int.MinValue + "..." + int.MinValue;
 		}
 
 		private void label4_Click(object sender, EventArgs e)
 		{
-
+			label_float.Text = float.MinValue + "..." + float.MaxValue;
 		}
 	}
 }
